ddef97332575bb3a3dce722161f54d2cbc822432
Arunodyawak <127820118+Arunodyawak@users.noreply.github.com>
2025-06-28 04:23:34 +0530

    Create Values section

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,19 +1,70 @@
 import React from 'react'
+import Heading from '../Heading/Heading'
+import FruitsVegCat from '../../assets/fruits-and-veggies.png'
+import SeaFoodCat from '../../assets/meat-and-seafood.png'
+import DairyCat from '../../assets/dairy-and-eggs.png'
+import Button from '../Button/Button'
 
 const Category = () => {
+
+  const renderCards = category.map(card=>{
+
+    return(
+        // Card
+        <div className='flex-1 basis-[300px]'key={card.id}>
+            <div className=' w-full min h-[30vh] relative -mb-10 '>
+                {/* Crad Image */}
+                <img src={card.image} className='absolute bottom-0'/>
+            </div>
+            {/* Card Content */}
+            <div className='bg-zinc-100 pt-17 p-8 rounded-xl'>
+                <h3 className='text-zinc-800 text-3xl font-bold'>{card.title}</h3>
+                <p className='text-zinc-600 text mt-3 mb-9'>{card.description}</p>
+                <Button content="See All"/>
+            </div>
+        </div>
+    )
+  })  
+
+
   return (
     <section>
-        <div className='py-20'>
-            <div className='w-fit mx-auto'>
-                
-            <h2 className='md:text-5xl text-[2.5rem] font-bold'>
-                <span className='text-orange-400'>Shop</span> by Category
-                </h2>
-                <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto'></div>
-            </div>
+        <div className='max-w-[1400px] mx-auto px-10 py-20'>
+           <Heading highlight="Shop" heading="by Category" /> 
+
+           {/* Category Card */}
+           <div className='flex flex-wrap gap-10 md:mt-15 mt-10' >
+            {renderCards}
+           </div>
         </div>
     </section>
   )
 }
 
 export default Category
+
+const category = [
+{
+    id:1,
+    title :'Fruits & Veggies',
+    description : 'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
+    image : FruitsVegCat,
+},
+
+{
+    id:2,
+    title :'Dairy & Eggs',
+    description : 'Wholesome dairy products and free-range eggs. From creamy milk and yogurt to artisanal cheeses.',
+    image : DairyCat,
+},
+
+{
+    id:3,
+    title :'Meat & SeaFood',
+    description : 'High quality, responsibly sourced meat and seafood.Choose from fresh cuts,marinated options, and more.',
+    image : SeaFoodCat,
+},
+
+
+
+]
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { IoMdSearch } from "react-icons/io";
@@ -8,17 +8,25 @@ import { TbMenu3 } from "react-icons/tb";
 const Navbar = () => {
 
 const [showMenu, setShowMenu] = useState(false);
-
+const [isScrolled, setIsScrolled] = useState(false);
 const toggleMenu =() => {
 
     setShowMenu (!showMenu);
 }
 
+useEffect(()=>{
 
+    const handleScroll = () => {
+        setIsScrolled(window.scrollY > 10)
+    }
+
+    window.addEventListener('scroll',handleScroll );
+    return () => window.removeEventListener('scroll', handleScroll)
+})
   return (
     
    
-        <header  className='bg-white fixed top-0 right-0 left-0'>
+        <header  className={`bg-white fixed top-0 right-0 left-0 z-50 ${isScrolled ? 'drop-shadow-[0_4px_25px_rgba(0,0,0,0.1)]' : ''}`}>
             <nav className='max-w-[1400px] mx-auto md:h-[14vh] h-[12vh] px-10 flex justify-between items-center'>
                 <a href="#" className='text-3xl font-bold'>
                     {/*Logo*/}
@@ -65,7 +73,7 @@ const toggleMenu =() => {
 
                  {/* Mobile menu */}
 
-                  <ul className={`flex flex-col gap-y-12 bg-orange-400/15 backdrop-blur-xl rounded-xl p-10 items-center gap-x-15 md:hidden absolute top-30 -left-full transform-translate-x-1/2 transition-all duration-500 ${showMenu ? 'left-1/2' : ""}`}>
+                  <ul className={`flex flex-col gap-y-12 bg-orange-400/15 backdrop-blur-xl shadow-xl rounded-xl p-10 items-center gap-x-15 md:hidden absolute top-30 -left-full transform-translate-x-1/2 transition-all duration-500 ${showMenu ? 'left-1/2' : ""}`}>
                     <li>
                         <a href="#"className='font-semibold tracking-wider  text-orange-500'>Home</a>
                     </li>
